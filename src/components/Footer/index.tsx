e89e43faafdb68bfdc70627bfd492c4c2a224077
genekellyboyle <DrDuffy@bhhsnv.com>
2026-01-05 01:16:03 -0800

    Update footer copyright to Berkshire Hathaway HomeServices Nevada Properties

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -292,24 +292,7 @@ const Footer = () => {
           <div className="h-px w-full bg-linear-to-r from-transparent via-[#D2D8E183] to-transparent dark:via-[#959CB183]"></div>
           <div className="py-8">
             <p className="text-center text-base text-body-color dark:text-white">
-              Template by{" "}
-              <a
-                href="http://uideck.com"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="hover:text-primary"
-              >
-                UIdeck
-              </a>{" "}
-              and{" "}
-              <a
-                href="https://nextjstemplates.com"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="hover:text-primary"
-              >
-                Next.js Templates
-              </a>
+              © 2025 Berkshire Hathaway HomeServices Nevada Properties - New Construction Homes Division. Comprehensive Real Estate Services by Dr. Jan Duffy S.0197614. All rights reserved.
             </p>
           </div>
         </div>
